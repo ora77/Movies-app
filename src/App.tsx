10ca404b4103b58865e7d7ae918408cd38024218
ora77 <olivier.abdelmoula@gmail.com>
2023-05-25 15:58:31 +0200

    Detail component added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './shared/Header';
 import { HomePage } from "./pages/homePage/HomePage";
+import { Details } from './pages/homePage/components/Details';
 
 
 function App() {
---
+++ src/pages/homePage/components/Details.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Details = () => {
+  return (
+    <>
+      <h1>title</h1>
+      <p>synopsis</p>
+      <p>year</p>
+      <img />
+    </>
+  );
+};
