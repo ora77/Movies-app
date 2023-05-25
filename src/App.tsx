badc811f10e89c51caf05fb48383bed074d638fe
Suleman <suleman.khn10@gmail.com>
2023-05-25 15:19:10 +0200

    resolved

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './shared/Header';
+import { HomePage } from "./pages/homePage/HomePage";
+
 
 function App() {
   return (
     <Header/>
   );
-
-import React from "react";
-import { HomePage } from "./pages/homePage/HomePage";
-
-
-function App() {
-  return <div></div>;
-
 }
 
 export default App;
