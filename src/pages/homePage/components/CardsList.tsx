187a6b5c9e93d0c47cea2a01d98330757561bbeb
ora77 <olivier.abdelmoula@gmail.com>
2023-05-30 11:23:57 +0200

    test

--- src/pages/homePage/components/CardsList.tsx
+++ src/pages/homePage/components/CardsList.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import './CardsList.css'
+import "./CardsList.css";
 
-import { Card } from "./Card"
-import { Movie } from "../../../models/Movie"
+import { Card } from "./Card";
+import { Movie } from "../../../models/movie";
 
-interface MoviesProps{
-    movies: Movie[],
+interface MoviesProps {
+  movies: Movie[];
 }
 
-export const CardsList = ({movies}: MoviesProps) => {
-
-   return (
-     <div className='CardsList'>
-       {movies.map((movie) => (
-         <div key={movie.id}>
-           <Card src={movie.poster_path} title={movie.title} />
-         </div>
-       ))}
-     </div>
-   );
+export const CardsList = ({ movies }: MoviesProps) => {
+  return (
+    <div className="CardsList">
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <Card src={movie.poster_path} title={movie.title} />
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/pages/homePage/components/Details.tsx
+++ src/pages/homePage/components/Details.tsx
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Details.css";
-import { getMovieById } from "../../../api/Movie";
+import { getMovieById } from "../../../api/movie";
 import { useParams } from "react-router-dom";
-
+import { Movie } from "../../../models/movie";
 
 export const Details = () => {
-  interface Details {
-    id: number;
-    title: string;
-    runtime: number;
-    release_date: string; // example : "1958-12-28"
-    genres: { name: string }[];
-    overview: string;
-    poster_path: string;
-  }
-
-  const [movie, setMovie] = useState<null | Details>(null); // remplacer "Details" par ...?
+  const [movie, setMovie] = useState<null | Movie>(null); // remplacer "Details" par ...?
 
   const { id } = useParams();
-
 
   useEffect(() => {
     const test = async () => {
@@ -30,13 +19,11 @@ export const Details = () => {
     test();
   }, []);
 
-  const genresArray = movie?.genres.map((x) => x.name);
-
   if (movie == null) return <p>Loading...</p>;
 
   return (
     <main className="detail-main">
-      <h1 className="detail-title">{movie.title}</h1>
+      <h2 className="detail-title">{movie.title}</h2>
       <ul className="detail-list ">
         <li>{movie.release_date.slice(0, 4)}</li>
         {/* methode sur la chaine de caractere pour avoir l'année? */}
@@ -48,14 +35,12 @@ export const Details = () => {
           src={`${movie.poster_path}`} //????
         />
       </figure>
-      <ul className="detail-list genres">{genresArray}</ul>
+      <ul className="detail-list genres">
+        {movie.genres.map((x) => (
+          <li>x.name</li>
+        ))}
+      </ul>
       <p className="detail-synopsis">{movie.overview}</p>
     </main>
-
-  )
-}
-
-
-
- 
-
+  );
+};
